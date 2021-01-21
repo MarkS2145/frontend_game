122cb89b5a46fb426425d7934ba82bddc813537a
MarkStuebs <markmewords@hotmail.com>
2021-01-21 11:04:00 -0800

    Implemented out of bounds checks for each side of the grid that kills the snake.  Next check to implement is if we try to eat ourselves.

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ debug ? console.log('hello world') : null;
    2) Used for error bounds checking of head position within grid
 */
 const GRID_SIZE = 5; 
+/*
+    HEADING_direction consts are used for boundary checking to ensure snake stays within bounds.
+    as teh GRID_SIZE determines number of rows/columns heading left or right ina  row is one step.
+    Theirfore, 
+        HEADING_RIGHT = +1, HEADING_LEFT = -1;
+        heading Up or Down is jumping rows so your offset is the grid size.
+        HEADING_UP = -GRID_SIZE, HEADING_DOWN = GRID_SIZE;
+*/
+const HEADING_RIGHT = 1, HEADING_LEFT = -1;
+const HEADING_UP = -GRID_SIZE, HEADING_DOWN = GRID_SIZE;
+
+console.log( `Heading Up ${HEADING_UP} down ${HEADING_DOWN}`);
+
 
 
 // Game Global Variables
@@ -20,7 +33,7 @@ let statusMsg; // status msg Used for outputting the results of the game
 
 let playAgainButton;// For now I will keep the button, but expect to lose it.  ENTER to start
 
-let moves = [];
+// let moves = [];
 
 debug ? console.log(allCells) : null;
 
@@ -28,14 +41,14 @@ debug ? console.log(allCells) : null;
 
 
 // Used to keep track of score and then test for a winner
-let gameWinner ;
-let row= [ 0, 0, 0 ]; // ROW ORDER 1, 2, 3, Top to bottom.
-let col = [ 0, 0, 0]; // ROW ORDER 1, 2, 3, Top to bottom.
-let diag = [ 0, 0 ]; // Diaginal ORDER 1, Top left to bottom right, 2 top right to bottom left
+// let gameWinner ;
+// let row= [ 0, 0, 0 ]; // ROW ORDER 1, 2, 3, Top to bottom.
+// let col = [ 0, 0, 0]; // ROW ORDER 1, 2, 3, Top to bottom.
+// let diag = [ 0, 0 ]; // Diaginal ORDER 1, Top left to bottom right, 2 top right to bottom left
 
 // Use variable to count number of valid moves so we can test
 // if we have  draw and stop the game
-let gValidMove = 0;
+// let gValidMove = 0;
 
 //
 
@@ -75,183 +88,152 @@ function setUp () {
 
 // KeyDown identifies the arrow keys for us which will direct snake
 function keyDown(event) {
-    
+    // Save off existing head value
+    let currentHead = head;
+
     debug ? console.log(event) : null ;
-    debug ? console.log("KeyCode:" + event.keyCode) : null ;
-    debug ? console.log("Head value: " + head) : null;
+    debug ? console.log("KeyCode: " + event.keyCode) : null ;
+    debug ? console.log("Current Head value: " + currentHead + "\n") : null;
     
     // Game starts
     if (head == null) {
+        
         allCells[12].classList.add('snake');
         head = parseInt(allCells[12].id);
+        console.log("Null head value assignment, after: " + head);
     }
 
+
+    let returnValue = null;
 
     // Update snake head location
     try {
         switch ( event.keyCode) {
             case 37 : 
                 console.log("Left Arrow"); 
+
+                returnValue = outOfBoundsCheck(head, HEADING_LEFT);
+
+                if ( returnValue ) {
+                    // we are out of bounds, call summary()
+                    summary("Death by Out of Bounds Left");
+                    break;
+                }
+
                 allCells[head].classList.remove('snake');
-                head = head - 1;
+                head = head + HEADING_LEFT;
                 allCells[head].classList.add('snake');
                 break;
             case 38 : 
                 console.log("Up Arrow");
+
+                returnValue = outOfBoundsCheck(head, HEADING_UP);
+
+                if ( returnValue ) {
+                    // we are out of bounds, call summary()
+                    summary("Death by Out of Bounds Up");
+                    break;
+                }
+
                 allCells[head].classList.remove('snake');
-                head = head - 5;
+                head = head + HEADING_UP;
                 allCells[head].classList.add('snake');
                 break;
             case 39 : 
                 console.log("Right Arrow"); 
-                if ( head == 4 || head == 9 || head == 14 || head == 19) {
-                    throw "Youre Dead"
+
+                returnValue = outOfBoundsCheck(head, HEADING_RIGHT);
+
+                if ( returnValue ) {
+                    // we are out of bounds, call summary()
+                    summary("Death by Out of Bounds Right");
+                    break;
                 }
+
                 allCells[head].classList.remove('snake');
-                head = head + 1;
+                head = head + HEADING_RIGHT;
                 allCells[head].classList.add('snake');
                 break;
     
             case 40 : 
                 console.log("Down Arrow"); 
+
+                returnValue = outOfBoundsCheck(head, HEADING_DOWN);
+
+                if ( returnValue ) {
+                    // we are out of bounds, call summary()
+                    summary("Death by Out of Bounds Down");
+                    break;
+                }
+                
                 allCells[head].classList.remove('snake');
-                head = head + 5;
+                head = head + HEADING_DOWN;
                 allCells[head].classList.add('snake');
                 break;
+            default:
+                console.log("Default hit in try/catch block of keydown")
         }
-
-    } // end of try
-    catch {
-        console.log("You're dead!")
-        summary("You're dead!")
+        debug ? console.log("New Head value: " + head) : null;     
+    }// end of try
+    catch (e) {
+    //     console.log("Caught you.. you're dead!")
+        summary("Catch:  You're dead! :Error:" + e.message )
+        console.error();
     }
     
 
 }
 
 
+function outOfBoundsCheck(currentHeadLocation, direction) {
 
-// Change color of grid cell when allowed, else msg.  Check for winner()
-function boxClicked(e) {
+    if (direction == null ) throw new UserException( "Out of bounds check received null direction");
+    if (currentHeadLocation == null ) throw new UserException( "Out of bounds check received null currentHeadLocation");
     
-    debug ? console.log(e) : null ;
-    
-    if (e.target.id != '' ) {
-
-        debug ? console.log("click ID: " + e.target.id + " bgColor: " + grid.children[e.target.id].style.backgroundColor ) : null ;
-
-        // grid.children[e.target.id].style.background = 'red';
-        if (grid.children[e.target.id].style.backgroundColor == 'lightgray') {
-            // We have a valid move, update counter
-            gValidMove++;
-            // Toggle the player color each turn, only when a valid move is made
-            grid.children[e.target.id].style.backgroundColor = playerColor;
-            
-            if (playerColor == 'red') {
-                grid.children[e.target.id].innerText = -1;
-                grid.children[e.target.id].style.color = 'red';
-                playerColor = 'blue' ;
-                message.innerText = playerColor + " move" ;
-                //Call to check win()
-                updateScore(e.target.id, -1);
-            }
-            else if ( playerColor == 'blue' ) {
-                grid.children[e.target.id].innerText = 1;
-                grid.children[e.target.id].style.color = 'blue';
-                playerColor = 'red' ;
-                message.innerText = playerColor + " move" ;
-                //Call to check win()
-                updateScore(e.target.id, 1 );
-            }
-            else {
-                debug ? console.log("square taken, try again") : null ;
-            }
-            
-            
-            
-        }
-    }
-
-}
-
-// updateScore sums rows columns and diagnoals looking for an abs score of 3
-//  if three is found, then the sign determines the winner
-//      red is negative, blue positive
-function updateScore(scoreTarget, currentValue){
-
-    debug ? console.log("winner() called" + scoreTarget + "currentValue: " + currentValue ) : null ;
-        
-
-    switch ( scoreTarget )  {
-        case 'c1r1' : 
-        col[0] += currentValue; row[0] += currentValue;  diag[0] += currentValue; break;
-        case 'c1r2' : 
-        col[0] += currentValue; row[1] += currentValue;  break;
-        case 'c1r3' : 
-        col[0] += currentValue; row[2] += currentValue;  diag[1] += currentValue; break;
-        case 'c2r1' : 
-        col[1] += currentValue; row[0] += currentValue;  break;
-        case 'c2r2' : 
-        col[1] += currentValue; row[1] += currentValue;  diag[0] += currentValue; diag[1] += currentValue; break;
-        case 'c2r3' : 
-        col[1] += currentValue; row[2] += currentValue;  break;
-        case 'c3r1' : 
-        col[2] += currentValue; row[0] += currentValue;  diag[1] += currentValue; break;
-        case 'c3r2' : 
-        col[2] += currentValue; row[1] += currentValue;  break;
-        case 'c3r3' : 
-        col[2] += currentValue; row[2] += currentValue;  diag[0] += currentValue; break;
-        
-    }
-    //}
-    debug ? console.log("row: " + row + "col: " + col + "diag: " + diag) : null ;
-
-    checkForWinner(row, col, diag);  // Pass in arrays
-
-} // end of updateScore()
-
-// checkForWinner() holds the winning combinations to check against
-function checkForWinner(row, col, diag) {
-
+    /* Direction is passed in as:
+        HEADING_RIGHT = +1, HEADING_LEFT = -1;
+        heading Up or Down is jumping rows so your offset is the grid size.
+        HEADING_UP = -GRID_SIZE, HEADING_DOWN = GRID_SIZE;
+    */
    
-    winmsg = "Winner! Congratulations ";
+    // Check if we are heading out of grid on the right hand side
+    if ( direction == HEADING_RIGHT ) {
+        for ( let i = 1, boundaryTest = 0; i <= GRID_SIZE ; i++) {
 
+            boundaryTest = (GRID_SIZE * i) - direction;
+        
+            // Check we aren't currently in the last cell of the right most column and heading right
+            if ( currentHeadLocation == (boundaryTest) ) { return true; }
+        }
+    }
 
-    for ( let i = 0 ; i < 3 ; i++ ) {
+    if ( direction == HEADING_LEFT ) {
+        // We need to test for incrementing outside of playing grid
+        for ( let i = 0, boundaryTest = 0; i < GRID_SIZE ; i++) {
 
-        if ( Math.abs(row[i]) == 3 ) {
-            // We have a winner
-            gameWinner = row[i];
+            boundaryTest = (GRID_SIZE * i) + HEADING_LEFT; 
+            
+            // Check we aren't currently in the last cell of a row
+            if ( currentHeadLocation + direction == (boundaryTest) ) { return true; }
         }
-        if ( Math.abs(col[i]) == 3 ) {
-           // We have a winner
-           gameWinner = col[i];
-       }
-       if ( i < 2) {
-           if ( Math.abs(diag[i]) == 3 ) {
-               // We have a winner
-               gameWinner = diag[i];
-           } 
-       }
-   }
-      
-    if( gameWinner < -2) {
-           // Red wins
-           summary(winmsg + "Red") ;
-        }
-    else if( gameWinner > 2) {
-            // else if blue wins
-            summary(winmsg + "Blue") ;
-        }
-    else if ( gValidMove >= 9 ) {
-            // Must be a tie if we have got here.  
-            // Stop game and ask to play again
-            summary("Ahh, tied game!") ;
-        }
+    }
+    
+    // Check only if we are heading off the top of the grid, negative value
+    if ( direction == HEADING_UP && currentHeadLocation + direction < 0 ) { return true; }
+
+    // Check only if we are heading off the bottom of the grid, values > gird.length
+    if ( direction ==  HEADING_DOWN && currentHeadLocation + direction > (allCells.length - 1) ) { return true; }
+
+    // All OK, go about our business
+    return false
 
 }
 
-//summary() provides a summary fo gameplay outcome and resets variables
+
+
+
+
+//summary() provides a summary of gameplay outcome and resets variables
 function summary(msg) {
 
     debug ? console.log("Summary called by" + msg) : null ;
@@ -266,7 +248,6 @@ function summary(msg) {
     //Reset values ready for the next game
     document.removeEventListener('keydown', keyDown);
 
-    
 }
 
 //playAgain() Sam is called by teh event click on the button to strat a new game
@@ -276,6 +257,10 @@ function playAgain() {
     statusMsg.innerText = "Hit any key to start"
 }
 
+function UserException(message) {
+    this.message = message;
+    this.name = 'UserException' ;
+}
 
 // Let's run the game
 setUp();
